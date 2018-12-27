d15ce3ea797abc0d51828d358733a2293d5dcfd6
KenichiroArai <sammyleejunior@gmail.com>
2018-12-27 15:59:51 +0900

    Sample.java
    サンプル

---
+++ araikenichiro.blog.fc2.com/番号ごと/00001/00009/JavaFXテンプレート/src/kmg/test/Sample.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package kmg.test;
+
+/**
+ * @author KenichiroArai
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+
+}
