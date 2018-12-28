4372ff4102ef1ef7e4c483500f81918a491fdca8
KenichiroArai <sammyleejunior@gmail.com>
2018-12-28 14:55:27 +0900

    KabaSampleJavaFx
    追加

---
+++ araikenichiro.blog.fc2.com/番号ごと/00001/00009/KabaSampleJavaFx/src/main/java/kmg/kaba/sample/java_fx/App.java
@@ -0,0 +1,13 @@
+package kmg.kaba.sample.java_fx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
