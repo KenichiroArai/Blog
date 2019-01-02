1d6956a6ed0fd907b2ddd5fd6a4448fc027443f5
KenichiroArai <sammyleejunior@gmail.com>
2019-01-02 16:16:22 +0900

    新井健一朗ブログ
    00011
    
    プロジェクトの作成

---
+++ araikenichiro.blog.fc2.com/番号ごと/00001/00011/MukaMukaSamplePoi/src/main/java/kmg/mukamuka/serivce/sample/poi/PoiSampleService.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+package kmg.mukamuka.serivce.sample.poi;
+
+/**
+ * POIサンプルサービスインタフェース
+ *
+ * @author KenichiroArai
+ */
+public interface PoiSampleService {
+    // 処理なし
+}
---
+++ araikenichiro.blog.fc2.com/番号ごと/00001/00011/MukaMukaSamplePoi/src/main/java/kmg/mukamuka/serivce/sample/poi/impl/PoiSampleServiceImpl.java
@@ -0,0 +1,22 @@
+package kmg.mukamuka.serivce.sample.poi.impl;
+
+import kmg.mukamuka.serivce.sample.poi.PoiSampleService;
+
+/**
+ * POIサンプルサービス
+ *
+ * @author KenichiroArai
+ */
+public class PoiSampleServiceImpl implements PoiSampleService {
+
+    /**
+     * エントリポイント
+     *
+     * @param args
+     *            オプション
+     */
+    public static void main(final String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
