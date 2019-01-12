a6aea906ee1c46fc7e58f3a06620b079c76dcca4
KenichiroArai <sammyleejunior@gmail.com>
2019-01-12 17:00:32 +0900

    新井健一朗のブログ
    00016
    
    プロジェクトの作成

---
+++ araikenichiro.blog.fc2.com/番号ごと/00001/00016/Seasar2Sample/Seasar2SampleMukamuka/src/main/java/kmg/mukamuka/sample/App.java
@@ -0,0 +1,13 @@
+package kmg.mukamuka.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
