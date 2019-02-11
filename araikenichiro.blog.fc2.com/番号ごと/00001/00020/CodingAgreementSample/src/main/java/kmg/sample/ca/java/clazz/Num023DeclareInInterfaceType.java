0e30f9b11a4daa32a254b2a5ae795225b38931d6
KenichiroArai <sammyleejunior@gmail.com>
2019-02-11 10:38:19 +0900

    新井健一朗のブログ
    00020
    
    サンプルソース実装中
    
    番号023：インタフェースの型で宣言する

---
+++ araikenichiro.blog.fc2.com/番号ごと/00001/00020/CodingAgreementSample/src/main/java/kmg/sample/ca/java/clazz/Num023DeclareInInterfaceType.java
@@ -0,0 +1,19 @@
+package kmg.sample.ca.java.clazz;
+
+import java.time.LocalDate;
+
+/**
+ * 番号023：インタフェースの型で宣言する
+ *
+ * @author KenichiroArai
+ */
+public interface Num023DeclareInInterfaceType {
+
+    /**
+     * 現在の日付を返す。
+     *
+     * @return 現在の日付
+     */
+    LocalDate getNow();
+
+}
